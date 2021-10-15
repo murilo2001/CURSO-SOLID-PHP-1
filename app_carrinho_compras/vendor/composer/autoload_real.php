76614fab32fa791c0c92deb9560859ff72e29ec1
Murilo Moreira <murilojosemoreira@hotmail.com>
2021-10-15 17:08:25 -0300

    havia esquecido de colocar .gitignore para ignorar a /vendor :(

--- app_carrinho_compras/vendor/composer/autoload_real.php
+++ app_carrinho_compras/vendor/composer/autoload_real.php
@@ -50,6 +50,24 @@ class ComposerAutoloaderInit0e99fe5c2a19ca6cf4be44a96676ec14
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit0e99fe5c2a19ca6cf4be44a96676ec14::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire0e99fe5c2a19ca6cf4be44a96676ec14($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire0e99fe5c2a19ca6cf4be44a96676ec14($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
